bd6a3d048dde71b62c7e45069de68ddc3caf33ee
hinzmann <hinzmann@cern.ch>
2020-10-01 16:08:43 +0200

    nnlo n6 root files

---
+++ chi_analysis/fastnlo/NNLO/download_v6.sh
@@ -0,0 +1,67 @@
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj0_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj0_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj0_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj0_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj1_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj1_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj1_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj1_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj2_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj2_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj2_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj2_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj3_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj3_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj3_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj3_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj4_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj4_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj4_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj4_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj5_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj5_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj5_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj5_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj6_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj6_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj6_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj6_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj7_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj7_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj7_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj7_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj8_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj8_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj8_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj8_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj9_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj9_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj9_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj9_chi_ct14nnlo_cppread_7_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjja_chi_ct14nlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjja_chi_ct14nlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjja_chi_ct14nnlo_cppread_0_m2.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjja_chi_ct14nnlo_cppread_7_m2.log
+
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj0_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj0_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj1_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj1_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj2_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj2_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj3_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj3_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj4_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O -N http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj4_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj5_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj5_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj6_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj6_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj7_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj7_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj8_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj8_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj9_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjj9_chi_ct14nnlo_cppread_7_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjja_chi_ct14nnlo_cppread_0_pt12.log
+curl --user Guest:2MyGuest -O http://ekpwww.etp.kit.edu/~rabbertz/fnlov2-Test/2jet.CMS13-ak04.fnl5662j/grids.v6/2jet.NNLO.fnl5662j_mjja_chi_ct14nnlo_cppread_7_pt12.log
